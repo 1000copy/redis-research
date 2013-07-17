737946daee9df8d9c0250a0c80b0f9eb774e0100
1000copy <1000copy@gmail.com>
2013-07-17 17:49:07 +0800

    -

---
+++ skiplist/skiplist/s1.h
@@ -0,0 +1,27 @@
+#define ZSKIPLIST_MAXLEVEL 32 /* Should be enough for 2^32 elements */
+#define ZSKIPLIST_P 0.25      /* Skiplist P = 1/4 */
+
+typedef struct redisObject {
+    unsigned type:4;
+    unsigned notused:2;     /* Not used */
+    unsigned encoding:4;
+    unsigned lru:22;        /* lru time (relative to server.lruclock) */
+    int refcount;
+    void *ptr;
+} robj;
+/* ZSETs use a specialized version of Skiplists */
+typedef struct zskiplistNode {
+    robj *obj;
+    double score;
+    struct zskiplistNode *backward;
+    struct zskiplistLevel {
+        struct zskiplistNode *forward;
+        unsigned int span;
+    } level[];
+} zskiplistNode;
+
+typedef struct zskiplist {
+    struct zskiplistNode *header, *tail;
+    unsigned long length;
+    int level;
+} zskiplist;
