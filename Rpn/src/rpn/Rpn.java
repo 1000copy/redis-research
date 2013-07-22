79b35d0cb218b15e4c7437e1407396eb3823bbad
1000copy <1000copy@gmail.com>
2013-07-22 14:50:15 +0800

    -

--- Rpn/src/rpn/Rpn.java
+++ Rpn/src/rpn/Rpn.java
@@ -1,21 +1,34 @@
 package rpn;
 
 import java.util.Stack;
+//"1. read src尾部加上 ）
+// 2. 在Stack内push一个（" 这样的表达式和原来的等效，但是不必分支判断stack是否非空。牛逼！
 public class Rpn {
+
     public static void main(String[] args) {
+        test_fix1();
+    }
+
+    private static void test_fix1() {
         Postfix p = new Postfix();
-        try{
-        System.out.println(p.toPostfix("1*(2+3)"));
-        System.out.println(p.toPostfix("1*(2+3+4)"));
-        System.out.println(p.toPostfix("1+2+3"));
-        }catch(Exception e ){
+        try {
+           System.out.print(p.toPostfix("10*(2+3)"));
+           System.out.println();
+            System.out.print(p.toPostfix("1*(2+3+4)"));
+           System.out.println();
+           System.out.print(p.toPostfix("1+2+3"));
+           System.out.println();
+           System.out.print(p.toPostfix("1*(2+3)").equals("123+*"));
+            System.out.print(p.toPostfix("1*(2+3+4)").equals("123+4+*"));
+            System.out.print(p.toPostfix("1+2+3").equals("12+3+"));
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 }
-//"1. read src尾部加上 ）
-// 2. 在Stack内push一个（" 这样的表达式和原来的等效，但是不必分支判断stack是否非空。牛逼！
+
 class Postfix {
+
     public int priority(char ch) {
         if (ch == '^') {
             return 3;
@@ -28,21 +41,28 @@ class Postfix {
         }
         return 0;
     }
-    public String toPostfix(String in)  {
+
+    public String toPostfix(String in) {
         String copy = in + ")";
         Stack s = new Stack();
         s.push('(');
-        int i, l = copy.length();
+        int i = 0, l = copy.length();
         char ch;
         String r = "";
-        for (i = 0; i < l; i++) {
+        while (i < l) {
             ch = copy.charAt(i);
-            if (isLetter(ch) == true) {
-                r += ch;
+            if (isLetter(ch)) {
+                while (isLetter(ch) && i < l) {
+                    r += ch;                    
+                    i++;
+                    ch = copy.charAt(i);
+                }
+                r += " ";
+                continue;
             } else if (ch == '(') {
                 s.push(ch);
             } else if (ch == ')') {
-                while (s.peek()!= '(') {
+                while (s.peek() != '(') {
                     r += s.pop();
                 }
                 s.pop();
@@ -52,17 +72,18 @@ class Postfix {
                 }
                 s.push(ch);
             }
+            i++;
         }
         return r;
     }
 
     private boolean isLetter(char ch) {
-        return  ch!='+' &&
-                ch!='-' &&
-                ch!='*' &&
-                ch!='/' &&
-                ch!='(' &&
-                ch!='^' &&
-                ch!=')' ;               
+        return ch != '+'
+                && ch != '-'
+                && ch != '*'
+                && ch != '/'
+                && ch != '('
+                && ch != '^'
+                && ch != ')';
     }
 }
