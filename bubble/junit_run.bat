965fab78c3622a1f80aac6cc9e9ce55d00390b7f
1000copy <1000copy@gmail.com>
2013-08-05 13:34:51 +0800

    change prompt windows + errorlevel handler

--- bubble/junit_run.bat
+++ bubble/junit_run.bat
@@ -1,12 +1,14 @@
 echo off 
+set prompt=~
 set CLASSPATH=..\lib\junit.jar;..\lib\hamcrest.jar;.
 set old=%path%
 set path=%path%;"C:\Program Files\Java\jdk1.7.0_25\bin\"
 rem 约定 java source 的编码为 utf8
 javac -encoding utf8 TestAll.java TestBubble.java Bubble.java
+if %errorlevel% neq 0 exit /b
 echo on
 java org.junit.runner.JUnitCore  TestAll
 echo off
 set path=%old%
 del *.class
-pause ...
+
