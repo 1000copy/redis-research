dc6135207fd1ca3f917535f0aa9e132107cd4d48
1000copy <1000copy@gmail.com>
2013-08-13 17:31:40 +0800

    3层循环，晕死了。

--- bubble/ShellSort.java
+++ bubble/ShellSort.java
@@ -9,6 +9,43 @@ public class ShellSort{
 	     } 
 	     return gap ;
 	}
+	public int[] _gap_step(int[] a)
+	{
+	     int n = a.length;
+	     int[] results = new int[a.length];
+	     int gap = _gap(n);
+	     int i = 0 ;
+	     while (gap > 0) 
+	     {	        
+	     	 results[i++] = gap ;
+	         gap = ( gap - 1 ) / 3;
+	     }    	     
+	     return java.util.Arrays.copyOfRange(results,0,i--);
+	 }
+	// int[] src = {3,4,2,5,1,6,8,9,10,7};
+	gap   4 ,4-10
+	public int[] _run(int[] a)
+	{
+	     int n = a.length;
+	     int i, j, temp; 
+	     int[] gaps = _gap_step(a);
+	     for(int gap : gaps)
+	     {
+	         for ( i = gap; i < n; i++ )
+	         {
+	             j = i - gap;
+	             // 分组排序
+	             temp = a[i];             
+	             while (( j >= 0 ) && ( a[j] > temp ))
+	             {
+	                 a[j + gap] = a[j];
+	                 j = j - gap;
+	             }
+	             a[j + gap] = temp;
+	         }	     
+	     }    	 	 
+	     return a ;
+	}
  	public int[] run(int[] a)
 	{
 	     int n = a.length;
--- bubble/TestShellSort.java
+++ bubble/TestShellSort.java
@@ -7,6 +7,12 @@ public class TestShellSort extends TestCase  {
         int[] src = {3,4,2,5,1,6,8,9,10,7};
         c.run(src); 
         assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10},src); 
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10},src); 
         assertEquals(13,c._gap(10)); 
+        assertArrayEquals(new int[]{13,4,1}, c._gap_step(src));
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10},c._run(src));
+        // for(int gap:new int[]{13,4,1}){
+        // 	System.out.println(gap);
+        // }
     } 
 }
